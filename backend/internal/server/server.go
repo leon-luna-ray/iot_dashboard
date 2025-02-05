52b6e391146a506882f7e65e47f525559e14e1df
Ray Luna <leon.luna.ray@gmail.com>
2025-02-04 20:48:23 -0800

    update instructions

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -3,12 +3,24 @@ package server
 import (
 	"iot_dashboard/internal/config"
 	"log"
+	"net"
 	"net/http"
 )
 
+func getOutboundIP() string {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return "localhost"
+	}
+	defer conn.Close()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP.String()
+}
+
 func Start(cfg *config.Config, router http.Handler) error {
-	log.Printf("hello world 🌐")
-	log.Printf("Server started at http://localhost:%s 📡\n", cfg.Port)
-	log.Printf("QP_API_BASE: %s", cfg.QPAPIBase)
+	ip := getOutboundIP()
+	log.Printf("Server started 📡")
+	log.Printf("(💻 Local):  http://localhost:%s", cfg.Port)
+	log.Printf("(🌐 Network):  http://%s:%s", ip, cfg.Port)
 	return http.ListenAndServe(":"+cfg.Port, router)
 }
